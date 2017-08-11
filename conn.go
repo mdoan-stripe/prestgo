862a5ba5c72f079ba8d0187c94a5338946688b94
Andrew Bullen <abull@stripe.com>
2017-08-11 12:35:39 -0700

    gofmt and remove no longer used var

--- conn.go
+++ conn.go
@@ -25,8 +25,7 @@ const (
 	DefaultUsername = "prestgo"
 
 	TimestampFormat = "2006-01-02 15:04:05.000"
-	TimeFormat = "15:04:05"
-	DateFormat = "2006-01-02"
+	DateFormat      = "2006-01-02"
 )
 
 var (
--- types.go
+++ types.go
@@ -48,7 +48,6 @@ const (
 	//Instant in time that includes the date and time of day without a time zone. Values of this type are parsed and rendered in the session time zone.
 	TimeWithTimezone = "time with time zone"
 
-
 	// Instant in time that includes the date and time of day without a time zone. Values of this type are parsed and rendered in the session time zone.
 	// Example: TIMESTAMP '2001-08-22 03:04:05.321'
 	Timestamp = "timestamp"
